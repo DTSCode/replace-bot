698feb843fca880756572ab7357205c8fdb7feec
Nick Chambers <DTSCode@gmail.com>
2015-03-28 01:53:43 -0700

    Update bot.pl

--- bot.pl
+++ bot.pl
@@ -6,8 +6,8 @@ use IO::Socket;
 my $server = "irc.freenode.net";
 my $nick = "replace-bot";
 my $login = "replace-bot";
-my $channel = "channels go here";
-my $sock = new IO::Socket::INET($server, 6667, 'tcp') or die "Can't Connect.\n";
+my $channel = "#cplusplus.com";
+my $sock = IO::Socket::INET->new(PeerAddr => $server, PeerPort => 6667, Proto => "tcp", Type => SOCK_STREAM) or die "Can't Connect.\n";
 
 print $sock "NICK $nick\r\n";
 print $sock "USER $login * * :Search and Replace Bot\r\n";
@@ -36,7 +36,7 @@ while(my $input = <$sock>) {
 
     my @msg = split ' ', $input, 4;
 
-    if($msg[1] =~ /^PRIVMSG/i) {
+    if($msg[1] =~ /^PRIVMSG$/i) {
         if($msg[3] =~ /^:\/s\/(.+?)\/(.+?)\//i or $msg[3] =~ /^:s\/(.+?)\/(.+?)\//i) {
             my $find = quotemeta $1;
             my $replace = $2;
@@ -44,13 +44,13 @@ while(my $input = <$sock>) {
             print $sock "PRIVMSG $msg[2] :$prevnick meant: $previnput\r\n";
         }
 
-        elsif($msg[3] =~ /^:!(help|info|about)/i) {
-            print $sock "PRIVMSG $msg[2] :$nick is a bot to search and replace accidental text.\r\n"
+        elsif($msg[3] =~ /^:!(help|info|about)$/i) {
+            print $sock "PRIVMSG $msg[2] :$nick is a bot to search and replace accidental text.\r\n";
             print $sock "PRIVMSG $msg[2] :Usage: /s/text to find/text to replace/ (Note that the / at the beginning is optional.\r\n";
         }
 
-        elsif($msg[3] =~ /^:!source/) {
-            print $sock "PRIVMSG $msg[2] :Source: \r\n";
+        elsif($msg[3] =~ /^:!source$/) {
+            print $sock "PRIVMSG $msg[2] :Source: https://github.com/DTSCode/replace-bot\r\n";
         }
 
         $prevnick = substr $msg[0], 1, index($msg[0], '!') - 1;
